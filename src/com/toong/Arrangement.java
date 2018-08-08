e5ac062e2d0c629808cda913ef6575c272f72a8d
Phan Van Linh <phanvanlinh.94vn@gmail.com>
2018-08-08 11:27:18 +0700

    Update some comments

---
+++ src/com/toong/Arrangement.java
@@ -0,0 +1,8 @@
+package com.toong;
+
+public class Arrangement {
+
+    public static void main(String[] args) {
+        char[] str = new char[]{'1', '2', '3', '4'};
+    }
+}
--- src/com/toong/Permutation.java
+++ src/com/toong/Permutation.java
@@ -3,26 +3,34 @@ package com.toong;
 public class Permutation {
 
     public static void main(String[] args) {
-        char[] str = new char[]{'1', '2', '3'};
+        char[] str = new char[]{'1', '2'};
         int len = str.length;
 
-        Permutation permutation = new Permutation();
-        permutation.permute(str, 0, len - 1);
+        permute(str, 0, len - 1);
     }
 
-    private void permute(char[] str, int start, int end) {
-        if (start == end)
+    private static void permute(char[] str, int start, int end) {
+        if (start == end) {
+            System.out.print("result = ");
             System.out.println(str);
-        else {
+        } else {
             for (int i = start; i <= end; i++) {
+//                System.out.print("before swap = start:" + start + " i=" + i + " end = " + end+ " ");
+//                System.out.println(str);
                 swap(str, start, i);
+                System.out.print("before permute = start:" + start + " i=" + i + " end = " + end + " arr=");
+                System.out.println(str);
                 permute(str, start + 1, end);
+//                System.out.print("after permute = start:" + start + " i=" + i + " end = " + end);
+//                System.out.println(str);
                 swap(str, start, i);
+//                System.out.print("after swap = start:" + start + " i=" + i + " end = " + end);
+//                System.out.println(str);
             }
         }
     }
 
-    private void swap(char[] str, int i, int j) {
+    private static void swap(char[] str, int i, int j) {
         char temp = str[i];
         str[i] = str[j];
         str[j] = temp;
